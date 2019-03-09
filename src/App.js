93a7660b5b78a3bf4549325a344c2c13225d809c
Kevin Dyke <krdyke@gmail.com>
2019-03-08 21:08:41 -0600

    Revert "push and worry about it later"
    This reverts commit 1f1a8bf643de56c98a9c9a8a3935c2ce8478bb2e.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import MapView from './MapView';
 import './App.css';
-
 
 class App extends Component {
   render() {
@@ -13,9 +11,7 @@ class App extends Component {
         </header>
         <section className="App-sidebar">
           <h1>sidebar</h1>
-          <div className="text-search"></div>
 
-         {/* <div className="foo"></div>
           <div className="foo"></div>
           <div className="foo"></div>
           <div className="foo"></div>
@@ -25,11 +21,11 @@ class App extends Component {
           <div className="foo"></div>
           <div className="foo"></div>
           <div className="foo"></div>
-          <div className="foo"></div>*/}
-        
+          <div className="foo"></div>
+          <div className="foo"></div>
         </section>
         <section className="App-map">
-          <MapView mapCenter={this.mapCenter}/>  
+          <h1>map</h1>
         </section>
       </div>
     );
