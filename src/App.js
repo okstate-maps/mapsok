0dc8ab2c9c58d115c3d1184a07fc41b0ce1da84e
Kevin Dyke <krdyke@users.noreply.github.com>
2019-05-15 14:58:18 -0500

    working on basic sql functionality

--- src/App.js
+++ src/App.js
@@ -2,22 +2,48 @@ import React, { Component } from 'react';
 import Config from './Config';
 import MapView from './MapView';
 import axios from 'axios';
+import URLSearchParams from '@ungap/url-search-params' //URLSearchParams polyfill
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-
+    //axios.defaults.headers.post['Content-Type'] = 'application/json';
     this.carto_user = Config.carto_user;
     this.carto_table = Config.carto_table;
     this.carto_table_fields = Config.carto_table_fields;
     this.carto_base_api = Config.carto_base_api;
-    this.execute_sql = this.execute_sql.bind(this);
     this.query_url = Config.carto_base_api.replace("{{username}}", Config.carto_user);
-    this.state = { "search_results": [] };
+    this.state = { "search_results": [], "base_features": [] };
+    this.execute_sql = this.execute_sql.bind(this);
+    //this.execute_sql();
+    this.initialize_query();
   }
 
+
+  initialize_query(){
+    let fields = this.carto_table_fields.join(", ");
+    let query = `SELECT ${fields} from ${this.carto_table}`;
+    console.log(query);
+    let that = this;
+    let callback = function(response) {
+      debugger;
+      that.setState({"base_features": response.data.features});
+    }
+    this.execute_sql(query, callback);
+  }
+
+
   execute_sql(query, callback, format){
+    
+    //just for testing////
+    //query = "SELECT title FROM okmaps2 LIMIT 10";
+    //format = "json";
+    if (!callback) {
+      callback = function(data){console.log(data)}
+    };
+    //////////////////////
+
     var format_str = '';
     if (format){
         format_str = format;
@@ -25,14 +51,21 @@ class App extends Component {
     else {
         format_str = "geojson";
     }
-    return (axios.post(this.carto_base_api, {
-        data: {
-          format: format_str,
-          q: query
-        },
-        type: "POST",
-      })
+
+    const params = new URLSearchParams();
+    params.append("q", query);
+    params.append("format", format_str);
+
+    return (axios.post(this.query_url, params,
+        {
+          transformRequest: [function(data, headers) { 
+              delete headers.common['Content-Type'];
+              return data; 
+          }]
+        }
+      )
         .then(callback)
+        //.then()
         .catch(function (error){
           console.log(error);
       })
