508f279c04e9d9b684e0da3f514ae34834a7d9ef
Kevin Dyke <krdyke@gmail.com>
2019-05-15 08:41:35 -0500

    update may 15

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Config from './Config';
+import MapView from './MapView';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.carto_user = Config.carto_user;
+    this.carto_table = Config.carto_table;
+    this.carto_table_fields = Config.carto_table_fields;
+    this.carto_base_api = Config.carto_base_api;
+    this.execute_sql = this.execute_sql.bind(this);
+    this.query_url = Config.carto_base_api.replace("{{username}}", Config.carto_user);
+    this.state = { "search_results": [] };
+  }
+
+  execute_sql(query, callback, format){
+    var format_str = '';
+    if (format){
+        format_str = format;
+    }
+    else {
+        format_str = "geojson";
+    }
+    return (axios.post(this.carto_base_api, {
+        data: {
+          format: format_str,
+          q: query
+        },
+        type: "POST",
+      })
+        .then(callback)
+        .catch(function (error){
+          console.log(error);
+      })
+    )
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,21 +47,18 @@ class App extends Component {
         </header>
         <section className="App-sidebar">
           <h1>sidebar</h1>
-
-          <div className="foo"></div>
-          <div className="foo"></div>
-          <div className="foo"></div>
-          <div className="foo"></div>
-          <div className="foo"></div>
-          <div className="foo"></div>
-          <div className="foo"></div>
-          <div className="foo"></div>
-          <div className="foo"></div>
-          <div className="foo"></div>
-          <div className="foo"></div>
+          <div className="flexlist">
+            <div className="foo"></div>
+            <div className="foo"></div>
+            <div className="foo"></div>
+            <div className="foo"></div>
+            <div className="foo"></div>
+            <div className="foo"></div>
+            <div className="foo"></div>
+          </div>
         </section>
         <section className="App-map">
-          <h1>map</h1>
+          <MapView />  
         </section>
       </div>
     );
